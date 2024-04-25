6a77af6a76203d95aed0cb52fcf8def2903a0dcd
Hiwotgebre <hiwotg173@gmail.com>
2024-04-25 16:19:33 -0400

    adding a function that takes an array of numbers and return the sum  into .js file

--- JS/function.js
+++ JS/function.js
@@ -1,4 +1,14 @@
-//  Thinking Functionally
+//  Paret: 1 Thinking Functionally
+// Take an array of numbers and return the sum
+
+let nums = [2, 4, 6, 8];
+
+function sumArray(nums) {
+    return nums.reduce((total, num) => total + num, 0);
+}
+
+console.log(sumArray(nums));
+
 // Take
 
 
